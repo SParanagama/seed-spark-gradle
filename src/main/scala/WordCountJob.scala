d73428d7eabfc5cf357ae2eade30c7f80dfb441a
Sagara Paranagama <sagara89dev@gmail.com>
2019-12-02 13:47:09 +1100

    Minor refactoring for understandability

--- src/main/scala/WordCountJob.scala
+++ src/main/scala/WordCountJob.scala
@@ -5,15 +5,19 @@ import org.apache.spark.sql.SparkSession
   */
 object WordCountJob {
   def main(args: Array[String]): Unit = {
+
+    val inputDataLocation = args(0)
+    val outputResultLocation = args(1)
+
     val ss = SparkSession.builder()
                          .master("local[*]")
                          .getOrCreate()
 
-    val textFile = ss.sparkContext.textFile(args(0))
+    val textFile = ss.sparkContext.textFile(inputDataLocation)
     val counts = textFile.flatMap(line => line.split(" "))
                          .map(word => (word, 1))
                          .reduceByKey(_ + _)
 
-    counts.saveAsTextFile(args(1))
+    counts.saveAsTextFile(outputResultLocation)
   }
 }
